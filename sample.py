8ba210dffa7e9b62598d4e14a0e2eb906f9b45e7
Jonghyun Park <parjong@gmail.com>
2026-01-27 22:08:35 +0900

    PoC: Automate Web Page Triage(?) with langchain
    [ci skip]
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

---
+++ sample.py
@@ -0,0 +1,71 @@
+from logging import getLogger
+from pprint import pp
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from pydantic import Field
+
+from bs4 import BeautifulSoup
+import requests
+
+
+logger = getLogger(__name__)
+
+
+class Page(BaseModel):
+    title: str = Field(description="The title of concise main title of the article or page")
+    date: str = Field(description="The issue or publication date as YYYY/MM/DD format (????/??/?? if unknown)")
+
+
+def get(url: str):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    full_content = response.text
+
+    soup = BeautifulSoup(full_content, 'html.parser')
+
+    title = soup.title.text.strip() if soup.title else "UNKNOWN"
+
+    logger.debug(title)
+    logger.debug(full_content)
+
+    return (response, title)
+
+
+def check(response) -> Page:
+    prompt = ChatPromptTemplate.from_template("""
+    Analyze the following content from a webpage and extract two pieces of information:
+    1. The concise main title of the article or page.
+    2. The issue or publication date as YYYY/MM/DD format (if available).
+       - If not available, state "????/??/??".
+
+    Format your answer as a JSON object with keys "date" and "title".
+
+    Content: {content}
+    """)
+
+    structured_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash").with_structured_output(Page)
+
+    chain = prompt | structured_llm
+
+    return chain.invoke({"content": response.text})
+
+cases = []
+cases.append("https://news.hada.io/topic?id=26122")
+#cases.append("https://evan-moon.github.io/2026/01/25/types-as-proofs-typescript-hidden-math/")
+#cases.append("https://huggingface.co/blog/nvidia/nemotron-speech-asr-scaling-voice-agents")
+#cases.append("https://zdnet.co.kr/view/?no=20260127135335")
+#cases.append("https://kr.linkedin.com/posts/geonho-shin-139155167_ax%EB%A5%BC-%EC%84%B1%EA%B3%B5%EC%8B%9C%ED%82%AC-%EC%88%98-%EC%9E%88%EB%8B%A4%EB%A9%B4-%EC%B0%A8%EB%9D%BC%EB%A6%AC-%EC%B0%BD%EC%97%85%EC%9D%84-%ED%95%98%EB%8A%94-%EA%B2%8C-%EB%82%AB%EB%8B%A4-%EC%A7%80%EB%82%9C-1%EB%85%84%EA%B0%84-activity-7421674203805859840-Pw3V")
+#cases.append("https://brunch.co.kr/@cmjung/16")
+#cases.append("https://block.github.io/goose/blog/2026/01/04/how-i-taught-my-agent-my-design-taste/")
+
+data= []
+
+for url in cases:
+    resp, title = get(url)
+    page = check(resp)
+    data.append({"url": url, "bs4.title": title, "gemini.date": page.date, "gemini.title": page.title})
+
+pp(data)
